3da6bd4175662917c5d8c4bd0b1a6e9c023ea4e4
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-08-13 17:59:23 +0200

    init

--- src/components/organizms/Navigation.jsx
+++ src/components/organizms/Navigation.jsx
@@ -2,16 +2,70 @@ import * as React from 'react'
 import { NavLink } from 'react-router-dom'
 import routes from '../../../src/routes'
 
-const Navigation = () => (
-  <div>
-    {routes.map(( route, i ) => (
-      <NavLink key={i} 
-        activeStyle={{color: 'red'}} 
-        to={route.path}>
-        {route.name}
-      </NavLink>
-    ))}
-  </div>
-)
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+// import MenuIcon from '@material-ui/icons/Menu'
 
-export default Navigation
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+}
+
+function ButtonAppBar(props) {
+  const { classes } = props
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            {/* <MenuIcon /> */}
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.flex}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+          {routes.map(( route, i ) => (
+            <NavLink key={i} 
+              activeStyle={{color: 'red'}} 
+              to={route.path}>
+              {route.name}
+            </NavLink>
+          ))}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+ButtonAppBar.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(ButtonAppBar);
+
+// const Navigation = () => (
+//   <div>
+    // {routes.map(( route, i ) => (
+    //   <NavLink key={i} 
+    //     activeStyle={{color: 'red'}} 
+    //     to={route.path}>
+    //     {route.name}
+    //   </NavLink>
+    // ))}
+//   </div>
+// )
+
+// export default Navigation
